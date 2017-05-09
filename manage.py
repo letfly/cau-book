897e783b6192a23f3c0f76d00e7fa58ecd970bde
letfly <letflykid@gmail.com>
2017-05-09 20:39:24 +0800

    Add port in manage

--- manage.py
+++ manage.py
@@ -4,11 +4,13 @@ from flask.ext.script import Manager, Server
 from book import app as web
 
 
+PORT = 5000
 app = web.create_app()
 manager = Manager(app)
 
-manager.add_command('server', Server(host='0.0.0.0', use_reloader=True))
+manager.add_command('server', Server(host='0.0.0.0', use_reloader=True, port=5000))
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
+    print "app started at port "+str(PORT)+"..."
     manager.run()
